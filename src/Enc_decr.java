462eac584c7e811aeed4a9c894a82ef92e1764ac
dnncomp <nickdragan@gmai.com>
2016-01-19 20:19:25 +0200

    Изменил процедуры шифрования и дешифрования для заглавных и малых букв отдельно. Но есть ошибка при рассшифровке (ab - не определяет)

--- src/Enc_decr.java
+++ src/Enc_decr.java
@@ -3,46 +3,68 @@ import java.math.*;
 //import java.lang.Number;
 
 public class Enc_decr {
-	
+
 	private static int firstKey = 5;
 	private static int secondKey = 19;
 	private static int module = 26;
 
-//	public static void main(String[] args) {
-//	    String input = "zorro";
-//	    String cipher = encrypt(input);
-//	    String deciphered = decrypt(cipher);
-//	    System.out.println("Source:    " + input);
-//	    System.out.println("Encrypted: " + cipher);
-//	   System.out.println("Decrypted: " + deciphered);
-//	}
+	// public static void main(String[] args) {
+	// String input = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	// String cipher = encrypt(input);
+	// String deciphered = decrypt(cipher);
+	// System.out.println("Source:    " + input);
+	// System.out.println("Encrypted: " + cipher);
+	// System.out.println("Decrypted: " + deciphered);
+	// }
 
 	static String encrypt(String input) {
-	    StringBuilder builder = new StringBuilder();
-	    for (int in = 0; in < input.length(); in++) {
-	        char character = input.charAt(in);
-	        if (Character.isLetter(character)) {
-	            character = (char) ((firstKey * (character - 'a') + secondKey) % module + 'a');
-	        }
-	        builder.append(character);
-	    }
-	    return builder.toString();
+		StringBuilder builder = new StringBuilder();
+		for (int in = 0; in < input.length(); in++) {
+			char character = input.charAt(in);
+			// if (Character.isLetter(character)) {
+			// character = (char) ((firstKey * (character - 'a') + secondKey) %
+			// module + 'a');
+			// }
+			if (Character.isLowerCase(character)) {
+				character = (char) ((((firstKey * character) + secondKey) % module) + 'a');
+			} else {
+				character = (char) ((((firstKey * character) + secondKey) % module) + 'A');
+			}
+			// character = (char) ((((firstKey * character) + secondKey) %
+			// module) + 'a');
+
+			builder.append(character);
+		}
+		return builder.toString();
 	}
 
 	static String decrypt(String input) {
-	    StringBuilder builder = new StringBuilder();
-	    // compute firstKey^-1 aka "modular inverse"
-	   BigInteger inverse = BigInteger.valueOf(firstKey).modInverse(BigInteger.valueOf(module));
-	    // perform actual decryption
-	    for (int in = 0; in < input.length(); in++) {
-	        char character = input.charAt(in);
-	        if (Character.isLetter(character)) {
-	           int decoded = inverse.intValue() * (character - 'a' - secondKey + module);
-	            character = (char) (decoded % module + 'a');
-	        }
-	        builder.append(character);
-	    }
-	    return builder.toString();
+		StringBuilder builder = new StringBuilder();
+		// compute firstKey^-1 aka "modular inverse"
+		BigInteger inverse = BigInteger.valueOf(firstKey).modInverse(
+				BigInteger.valueOf(module));
+		System.out.println(inverse.intValue());
+	
+		// perform actual decryption
+		for (int in = 0; in < input.length(); in++) {
+			char character = input.charAt(in);
+			if (Character.isLetter(character)) {
+//				 int decoded = inverse.intValue() * (character - 'a' -
+//				 secondKey + module);
+//				 character = (char) (decoded % module + 'a');
+				
+				if (Character.isLowerCase(character)) {
+					character = (char) (((inverse.intValue()
+							* ((character - secondKey)) % module)) + 'a');
+				} else {
+					character = (char) (((inverse.intValue()
+							* ((character - secondKey)) % module)) + 'A');
+				}
+
+			}
+			builder.append(character);
+		}
+		return builder.toString();
 	}
 
 }
